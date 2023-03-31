24d2c8343e18c9d994bd328a76c96a9f747fb7d0
BraedenHCL <braeden.norman6@gmail.com>
2023-03-30 17:06:16 -0700

    changed env variable setup to not crash if not set

--- app/api/config.py
+++ app/api/config.py
@@ -6,11 +6,17 @@ if "APP_ENV" in os.environ and os.environ['APP_ENV'] == 'production':
     hello_world = os.environ['HELLO_WORLD']
     health_check_endpoint = os.environ['HEALTH_CHECK_ENDPOINT']
     base_path = os.environ['BASE_PATH']
-
 else:
     load_dotenv('.env')
     hello_world = os.getenv('HELLO_WORLD')
     health_check_endpoint = os.getenv('HEALTH_CHECK_ENDPOINT')
     base_path = os.getenv('BASE_PATH')
+
+
+# this will ensure app run without error if the env variables are not set
+if health_check_endpoint == None:
+    health_check_endpoint = '/'
+if base_path == None:
+    base_path = ''
 
     
--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from app.api.endpoints import example
+from app.api.config import health_check_endpoint
 
 app = FastAPI()
 
 # add router from api/endpoints/example.py
 app.include_router(example.router)
 
-
-
-@app.get("/health-check")
+@app.get(health_check_endpoint)
 async def test():
     return "Ok"
